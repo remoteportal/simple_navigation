224558ee16dcebdd150099373ccfb75b25dea0b9
Peter Alvin <peter.alvin@gmail.com>
2020-03-04 15:45:47 -0500

    win: build 5:

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:simple_navigation/simple_navigation.dart';
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('home'));
+  }
+}
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  Nav.routes = {'/': Home()};
   runApp(new MyApp());
 }
 
@@ -11,9 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: Nav(
-        child: Scaffold(
-            body: Center(
-                child: Text('search me for search'))),
+//        child: Scaffold(body: Center(child: Text('search me for search'))),
       ),
     );
   }
--- lib/simple_navigation.dart
+++ lib/simple_navigation.dart
@@ -2,19 +2,51 @@ library simple_navigation;
 
 import 'package:flutter/material.dart';
 
-class Nav extends StatefulWidget {
-  Nav({this.child});
+class NavItem {
+  NavItem(this.route, [this.args]);
 
-  final Widget child;
+  String route;
+  Map<String, dynamic> args;
+}
+
+_NavState nav;
+
+class Nav extends StatefulWidget {
+  static NavItem pop() {
+    if (stack.length == 1) {
+      return stack.first;
+    } else {
+      return stack.removeLast();
+    }
+  }
+
+  static push(String route, [Map<String, dynamic> args]) {
+    stack.add(NavItem(route, args));
+  }
+
+  static Map<String, Widget> routes;
+  static List<NavItem> stack = [NavItem('/')];
+
+  static NavItem get top => stack.last;
+
+//  Nav({this.child});  // , this.routeMap
+  Nav(); // , this.routeMap
+
+//  final Widget child;
+
+//  final Map<String,Widget> routeMap;
 
   @override
-  _NavState createState() => _NavState();
+  _NavState createState() {
+    return nav = _NavState();
+  }
 }
 
 class _NavState extends State<Nav> {
   @override
   Widget build(BuildContext context) {
-    return Container(child:widget.child);
+//    return Container(child: widget.child);
+    return Container(child: Nav.routes['/']);
   }
 
   @override
